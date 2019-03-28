131f989f84591cc162b30cb66c42cb565dfb574c
Antwan Hommy <antwan.hommy@gmail.com>
2019-03-28 19:57:08 +0200

    added postgre as database and added run.py

--- application/__init__.py
+++ application/__init__.py
@@ -2,18 +2,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-
-
 # Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
 
-# Käytetään items.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, että tiedosto sijaitsee tämän sovelluksen tiedostojen 
-# kanssa samassa paikassa
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
+import os
 
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
-app.config["SQLALCHEMY_ECHO"] = True
+# Kun ollaan Herokussa, käytetään Herokun tietokantaa, muulloin omaa
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    # Käytetään items.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
+    # kertoo, että tiedosto sijaitsee tämän sovelluksen tiedostojen 
+    # kanssa samassa paikassa
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
+    # Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+    app.config["SQLALCHEMY_ECHO"] = True
 
 # Luodaan db-olio, jota käytetään tietokannan käsittelyyn
 db = SQLAlchemy(app)
@@ -49,4 +52,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # Luodaan lopulta tarvittavat tietokantataulut
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
