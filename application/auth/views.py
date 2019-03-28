74aa68b487a263c57952bdbb9ec0b15402890bb1
Antwan Hommy <antwan.hommy@gmail.com>
2019-03-28 18:18:56 +0200

    registering functionality added

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,26 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
+
+@app.route("/auth/register", methods = ["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("/auth/registerform.html", form = RegisterForm())
+    
+    form = RegisterForm(request.form)
+    #validoinnit tähän
+
+    if not form.validate:
+        return render_template("/auth/registerform.html", form = form)
+
+    u = User(form.name.data, form.username.data, form.password.data)
+    db.session().add(u)
+    db.session().commit()
+
+    return redirect(url_for("index"))
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -16,7 +33,7 @@ def auth_login():
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
         return render_template("auth/loginform.html", form = form,
-                                error = "Nu such username or password")
+                                error = "No such username or password")
 
     login_user(user)
     return redirect(url_for("index"))
