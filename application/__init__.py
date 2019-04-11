9a33bad71c8284af6123097ee13e91221a5f5772
Antwan Hommy <antwan.hommy@gmail.com>
2019-04-11 22:44:37 +0300

    added authorization

--- application/__init__.py
+++ application/__init__.py
@@ -23,6 +23,52 @@ db = SQLAlchemy(app)
 
 
 
+# Lisätään kirjautumisen toiminallisuus
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager, current_user
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please log in to use this functionality."
+
+
+
+# Määritellään rooleja tukeva dekoraattori
+from functools import wraps
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user:
+                return login_manager.unauthorized()
+          
+            if not current_user.is_authenticated:
+                return login_manager.unauthorized()
+            
+            unauthorized = False
+
+            if role != "ANY":
+                unauthorized = True
+                
+                for user_role in current_user.roles():
+                    if user_role == role:
+                        unauthorized = False
+                        break
+
+            if unauthorized:
+                return login_manager.unauthorized()
+            
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
+    
+
+
 # Luetaan kansiosta application tiedoston views sisältö
 from application import views
 
@@ -38,18 +84,6 @@ from application.auth import views
 from application.reviews import models
 from application.reviews import views
 
-
-# Lisätään kirjautumisen toiminallisuus
-from application.auth.models import User
-from os import urandom
-app.config["SECRET_KEY"] = urandom(32)
-
-from flask_login import LoginManager
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-login_manager.login_view = "auth_login"
-login_manager.login_message = "Please log in to use this functionality."
 
 @login_manager.user_loader
 def load_user(user_id):
--- application/items/views.py
+++ application/items/views.py
@@ -1,17 +1,17 @@
 from flask import render_template, request, url_for, redirect
-from flask_login import login_required, current_user
+from flask_login import current_user
 
-from application import app, db
+from application import app, db, login_required, login_manager
 from application.items.models import Item
 from application.items.forms import ItemForm
 
 @app.route("/items/new/")
-@login_required
+@login_required(role="ANY")
 def items_form():
     return render_template("items/new.html", form = ItemForm())
 
 @app.route("/items/", methods=["POST"])
-@login_required
+@login_required(role="ADMIN")
 def items_create():
     form = ItemForm(request.form)
 
@@ -32,15 +32,18 @@ def items_index():
     return render_template("items/list.html", items = Item.query.all())
 
 @app.route("/items/<item_id>/", methods = ["POST"])
-@login_required
+@login_required(role="ANY")
 def items_set_vegan(item_id): # VALITSE JOKU MUU KUIN DONE, MUOKKAA MODELS.PY
     i = Item.query.get(item_id)
+    if i.account_id != current_user.id:
+        return login_manager.unauthorized()
+
     i.vegan = True
     db.session().commit()
     return redirect(url_for("items_index"))
 
 @app.route("/items/<item_id>/delete", methods = ["POST"])
-@login_required
+@login_required(role="ANY")
 def item_delete(item_id):
     i = Item.query.get(item_id)
     db.session.delete(i)
