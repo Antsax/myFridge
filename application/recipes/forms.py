b5d30e71c53dfdec9bff0c1ab03ccb9591fc1147
Antsax <antwan.hommy@gmail.com>
2019-05-03 23:01:17 +0300

    added validation to recipe name

--- application/recipes/forms.py
+++ application/recipes/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import validators, StringField, IntegerField, TextAreaField, validators
 
 class RecipeForm(FlaskForm):
-    name = StringField("Recipe name", [validators.Length(min=2)])
+    name = StringField("Recipe name", [validators.Length(min=2), validators.required()])
     instructions = TextAreaField("Instructions", [validators.required(), validators.Length(min=50)])
     diet = StringField("Diet(s)", [validators.optional()])
     timeInMinutes = IntegerField("Time in minutes", [validators.optional()])
